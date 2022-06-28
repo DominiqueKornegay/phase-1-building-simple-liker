839dd952fc6266c2e511d1b2cfc2ee87849a8df1
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-06-27 21:13:02 -0400

    commit tree a92050b7b015932bf931733077be30fe77402395

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorModal = document.getElementById('modal')
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  errorModal.classList.add("hidden")
+  clickListener()
+})
 
 
+function hideError() {
+  errorModal.classList.add("hidden")
+}
 
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if (event.target.classList[0] === 'like-glyph') {
+      mimicServerCall()
+      .then(resp => {
+        const activated = event.target.classList.contains('activated-heart')
+        activated
+          ? event.target.classList.remove('activated-heart') : event.target.classList.add('activated-heart')
+      })
+      .catch(error => {
+        errorModal.classList.remove("hidden")
+        setTimeout(hideError, 3000)
+      })
+    }
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
